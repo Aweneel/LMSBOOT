23a234e3f799f2a3e9453883a29437b0b074edd0
anil <aweneelkc@gmail.com>
2018-10-17 00:22:28 -0400

    comment added for start-class

--- src/main/java/com/gcit/lms/LmsBootAngularJsApplication.java
+++ src/main/java/com/gcit/lms/LmsBootAngularJsApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
-
+//start-class
 @SpringBootApplication
 @EnableAutoConfiguration
 @ComponentScan("com.gcit.lms")
